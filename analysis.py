3300f66acdb2cd00c1ab365dbf4bca677bb3c059
Chris Browne <chrisbrowne92@hotmail.co.uk>
2020-07-08 21:01:25 +1000

    add reliability metrics to trendline

--- analysis.py
+++ analysis.py
@@ -11,13 +11,20 @@ mask = (data['book_rating_goodreads'] == -1) | (data['book_rating_goodreads'] ==
        (data['film_rating_imdb'] == -1) | (data['film_rating_imdb'] == -2)
 data = data[~mask]  # remove items where there is no rating
 
-ax = sns.regplot('book_rating_goodreads', 'film_rating_imdb', data=data)  # regression plot
+# regression plot
+ax = sns.regplot('book_rating_goodreads', 'film_rating_imdb', data=data)
 ax.set_xlim([0, 5])
 ax.set_ylim([0, 10])
-model = lin_reg(data['film_rating_imdb'], add_constant(data['book_rating_goodreads'])).fit()
-intercept, slope = model.params
-x = np.linspace(0, data['book_rating_goodreads'].min())
+
+# regression model to extrapolate line of best fit down to 0
+model = lin_reg(data['film_rating_imdb'], add_constant(data['book_rating_goodreads'])).fit()  # fit model
+intercept, slope = model.params  # get slope & intercept
+x = np.linspace(0, data['book_rating_goodreads'].min())  # x values for extrapolated lobf
 ax.plot(x, model.predict(add_constant(x)),
-        '--', color=ax.lines[0].get_color(), linewidth=ax.lines[0].get_linewidth())
-ax.lines[0].set_label('$y = {:.2f}x + {:.2f}$'.format(slope, intercept))
+        '--', color=ax.lines[0].get_color(), linewidth=ax.lines[0].get_linewidth())  # plot extrapolated lobf
+
+# p value & r-squared for label
+p_value = model.pvalues['book_rating_goodreads']
+rsquared = model.rsquared
+ax.lines[0].set_label('$y = {:.2f}x + {:.2f}$\n$p = {:.2f}$\n$R^2 = {:.2f}$'.format(slope, intercept, p_value, rsquared))
 plt.legend(loc='upper left')
